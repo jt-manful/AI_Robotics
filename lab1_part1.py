73736dc0268dfb0061213ab19300b85eb808110a
Joel Osei-Asamoah <joeloseifiifi@gmail.com>
2023-08-25 21:05:56 +0000

    adding functions for turning left and right

--- lab1_part1.py
+++ lab1_part1.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 # Left motor is output_c, and right_motor is output_b
-from ev3dev2.motor import LargeMotor, OUTPUT_C 
-
+from ev3dev2.motor import MoveTank, LargeMotor, OUTPUT_C, OUTPUT_B
+# multiplier = (circumference of turn) / (cicumference of wheel)
 def turnLeft(angle, speed):
-    pass
+    tank = MoveTank(OUTPUT_B, OUTPUT_C)
+    tank.on_for_degrees(left_speed=0, right_speed=speed, degrees=angle/multiplier)
+    print('Done!')
+
+def turnRight(angle,speed):
+    tank = MoveTank(OUTPUT_B, OUTPUT_C)
+    tank.on_for_degrees(left_speed=speed, right_speed=0, degrees=angle/multiplier)
+    print('Done!')
