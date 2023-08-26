71406096f148c4f46246b8a9bab3eb306e7c11e9
jt-manful <johnmanful2002@hotmail.com>
2023-08-26 00:06:50 +0000

    first commit for  26 aug

--- lab1_part1.py
+++ lab1_part1.py
@@ -2,6 +2,7 @@
 
 # Left motor is output_c, and right_motor is output_b
 from ev3dev2.motor import LargeMotor, MoveTank, OUTPUT_C, OUTPUT_B
+import math
 
 # initailize motors 
 left_motor = LargeMotor(OUTPUT_C)
@@ -16,16 +17,23 @@ def turnLeft(angle, speed):
 
 
 # Function takes an angle, a motor speed and a direction and causes the 
-# bot to spin either left or right.
+# bot to spin either left or right.'
 def spin(angle_deg, motor_speed, direction):
+
     tank = MoveTank(OUTPUT_B, OUTPUT_C)
+ 
+    # calculate actual turning angle (confirm calculations with joel)
+    baseline = 12.1 
+    distance_travelled = (2 * math.pi * baseline) / 360
+    actual_rotational_degrees = 0
+
 
     if not (0 >= motor_speed and motor_speed <= 1000):
         return -1
     
     if direction == 'right':
-            tank.on_for_degrees(left_speed=motor_speed, right_speed=-motor_speed, degrees=angle_deg)
+        tank.on_for_degrees(left_speed=-motor_speed, right_speed=motor_speed, degrees=actual_rotational_degrees)
     elif direction == 'left':
-        tank.on_for_degrees(left_speed=-motor_speed, right_speed=motor_speed, degrees=angle_deg)
+        tank.on_for_degrees(left_speed=motor_speed, right_speed=-motor_speed, degrees=actual_rotational_degrees)
     else:
          return -1
