8fab0663aef44dc73f82b80392c48f3725161027
Jordan <teyejordan@gmail.com>
2023-08-26 00:08:22 +0000

    driveStraight function added

--- lab1_part1.py
+++ lab1_part1.py
@@ -2,14 +2,25 @@
 
 # Left motor is output_c, and right_motor is output_b
 from ev3dev2.motor import LargeMotor, MoveTank, OUTPUT_C, OUTPUT_B
+import math
 
 # initailize motors 
 left_motor = LargeMotor(OUTPUT_C)
 right_motor = LargeMotor(OUTPUT_B)
+tank = MoveTank(left_motor_port=OUTPUT_C, right_motor_port=OUTPUT_B)
 
-
-def driveStraight():
-    pass
+# Distance is in centimeters
+# Motor speed is between -1000 and 1000
+def driveStraight(distance, speed):
+    # Distance
+    wheel_radius = 2.8 # centimeters
+    wheel_circumference = 2 * math.pi * wheel_radius
+    rotations_per_cent = 1/wheel_circumference
+    rotation = distance * rotations_per_cent
+    # Motor Speed
+    motor_speed = (speed/1000) * 100
+    
+    tank.on_for_rotations(motor_speed, motor_speed, rotation)
 
 def turnLeft(angle, speed):
     pass
@@ -18,7 +29,6 @@ def turnLeft(angle, speed):
 # Function takes an angle, a motor speed and a direction and causes the 
 # bot to spin either left or right.
 def spin(angle_deg, motor_speed, direction):
-    tank = MoveTank(OUTPUT_B, OUTPUT_C)
 
     if not (0 >= motor_speed and motor_speed <= 1000):
         return -1
