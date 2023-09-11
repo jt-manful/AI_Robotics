0f6e8e0019dfed78755ba7bc9c4cb7c9cf2cb435
Jordan <teyejordan@gmail.com>
2023-09-11 19:39:57 +0000

    pre-submission commit

--- LAB_3/path_planning.py
+++ LAB_3/path_planning.py
@@ -1,125 +1,156 @@
+#!/usr/bin/env python3
 
-# Initialize map
-world_map = [[0, 0, 0, 0, 0],
-             [0, 0, 1, 1, 0],
-             [0, 0, 1, 0, 0], 
-             [0, 1, 0, 0, 0]]
+# imports
+from planning_map import *
+from ev3dev2.motor import MoveTank, OUTPUT_C, OUTPUT_B
+import time
+import math
 
-world_map1 = [[0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0], 
-             [0, 0, 0, 0, 0]]
+# initailize motors
+tank = MoveTank(OUTPUT_B, OUTPUT_C)
 
-world_map2 = [[9, 8, 7, 6, 5],
-             [10, 9, 1, 1, 4],
-             [11, 10, 1, 4, 3], 
-             [12, 1, 4, 3, 2]]
+
+# global vars
+WHEEL_CIRCUMFRENCE = 19.5
+BASELINE = 14.6 
+SPIN_ANG = 90
+MOTOR_SPEED = 200
+LEFT = 'left'
+RIGHT = 'right'
+TILE_DISTANCE = 50.50
+DIAGONAL_DISTANCE = 71.5
+
+
+
+start_oreintation = 0
+
+
+# Names of cardinal directions corresponding to the integers 0, 1, 2, 3, 4, 5, 6, 7
+directions = ['east','south-east','south','south-west','west','noth-west','north','north-east']
+
+# Computes the direction of pos2 relative to pos1, if pos2 is adjacent to pos1
+# pos1 and pos2 are assumed to be tuples in the form (x,y)
+# Direction is represented as an integer between 0 (corresponding to east) and
+# 3 (corresponding to north)
+# Throws an exception if pos2 is not adjacent to pos1
+def relDirection(pos1, pos2):
+    (x1, y1) = pos1
+    (x2, y2) = pos2
+    if x2==x1 and y2==y1+1:
+        dir = 0
+    elif x2==x1+1 and y2==y1+1:
+        dir = 1
+    elif x2==x1+1 and y2==y1:
+        dir = 2
+    elif x2==x1+1 and y2==y1-1:
+        dir = 3
+    elif x2==x1 and y2==y1-1:
+        dir = 4
+    elif x2==x1-1 and y2==y1-1:
+        dir = 5
+    elif x2==x1-1 and y2==y1:
+        dir = 6
+    elif x2==x1-1 and y2==y1+1:
+        dir = 7
+    else:
+        raise ValueError(str(pos1)+" and " + str(pos2) + " are not neighbors,"\
+                         +"so cannot compute relative direction between them.")
+    return dir
+
+# Assuming the robot starts at startPosition, facing the direction startOrientation,
+# This function enables the robot to follow the path (a list of tuples representing
+# positions) stored in the parameter path.
+def followPath(startPosition, startOrientation, path):
+    curPos = startPosition
+    curDir = startOrientation
+
+    for i in range(len(path)):
+        nextPos = path[i]
+        relDir = relDirection(curPos, nextPos)
+        print("At pos " + str(curPos) + " facing direction " + str(curDir)
+              + " (" + directions[curDir] + ")")
+        print("Next pos is " + str(nextPos)
+              + ", whose direction relative to the current pos is "
+              + str(relDir) + " (" + directions[relDir] + ")")
+        print()
+              
+        # TO DO: IF NECESSARY, TURN TO FACE IN THE CORRECT DIRECTION
+        resultant_direction = curDir - relDir
+
+        if resultant_direction == 1 or resultant_direction == -7:
+            spin(45, MOTOR_SPEED, LEFT)
+        if resultant_direction == -1 or resultant_direction == 7:
+            spin(45, MOTOR_SPEED, RIGHT)
+        if resultant_direction == 2 or resultant_direction == -6:
+            spin(SPIN_ANG, MOTOR_SPEED, LEFT)
+        if resultant_direction == 6 or resultant_direction == -2:
+            spin(SPIN_ANG, MOTOR_SPEED, RIGHT)
+        if resultant_direction == 4 or resultant_direction == -4:
+            spin(180, MOTOR_SPEED, RIGHT)
+    
+         
+        # Update the current position and orientation
+        curPos = nextPos
+        curDir = relDir
+
+        # TO DO: MOVE ONE CELL FORWARD INTO THE NEXT POSITION
+        if curDir %2 == 1:
+            print("diagonal")
+            driveStraight(MOTOR_SPEED, DIAGONAL_DISTANCE)
+        else:
+            print("straight")
+            driveStraight(MOTOR_SPEED, TILE_DISTANCE)
+    
+       
+
+
+# Distance is in centimeters
+# Motor speed is between -1000 and 1000
+def driveStraight(speed, distance):
+    '''function takes in two params, distance and speed and causes the bot to drive
+      either forward or backward in in straight line for a given distance at a certain speed'''
+    # Distance
+    rotations_per_cent = 1/WHEEL_CIRCUMFRENCE
+    rotation = distance * rotations_per_cent
+    # Motor Speed
+    motor_speed = (speed/1000)*100
+    
+    if not ( -1000<= speed and speed <= 1000):
+        return -1
+    
+    tank.on_for_rotations(motor_speed, motor_speed, rotation)
+    
+    
+def spin(angle_deg, motor_speed, direction):
+    '''function takes an angle, a motor speed and a direction and 
+        causes the bot to spin either left or right a given angle for a given speed.'''
  
-start = (2, 1)
-goal = (3, 4)
+    # calculate actual turning angle 
+    half_baseline = BASELINE /2
+    multiplier = 2 * math.pi * half_baseline / WHEEL_CIRCUMFRENCE
+    actual_rotational_degrees = angle_deg * multiplier
 
-# Given the coordinates of a cell, you will want to be able 
-# to quickly compute the coordinates of its (up to) four neighboring cells.  
-# You should write a function to do this. 
-#  The function can take as a parameter a tuple representing the
-#  current position, and it can return a list of tuples representing the neighboring cells. 
+    motor_speed = (motor_speed/1000)*100
 
-
-
-def neighboring_cells(curPos, world_map):
-    '''The function  as a parameter: a tuple representing the current position,
-      and return a list of tuples representing the neighboring cells. '''
-   
-    neighboring_cells = []
-    map_y = len(world_map[0])
-    map_x = len(world_map)
-    curPos_x, curPos_y = curPos
-
-    for x in range(-1, 2):
-        for y in range(-1, 2):
-            if x == 0 and y == 0:
-                continue  
-            new_x, new_y = curPos_x + x, curPos_y + y
-            if 0 <= new_x < map_x and 0 <= new_y < map_y and world_map[new_x][new_y] != 1:
-                neighboring_cells.append((new_x, new_y))
-
-    return neighboring_cells
-
-
-
-
-def next_move(wavefront_map, startLoc):
-    ''' given a completed wavefront plan will repeatedly determine the next cell to traverse to,
-        by examining its neighboring cells to see which has the lower non-obstacle value.
-        It should add the coordinates of this cell to a list and also print them to the screen
-        (or play an audio representation), with an appropriate pause between each print statement 
-        to enable a human reader to follow what is happening.  The code stops when it adds the coordinates
-        of the goal cell to the list, and then it returns the list.  To test this code, you can manually
-        populate a 2D array with the values that would be generated by a wavefront planner.'''
+    if not (0 <=  motor_speed and motor_speed <= 1000):
+        return -1
     
-    nearby_cells = []
-    min_cell_value = wavefront_map[startLoc[0]][startLoc[1]]
-    path = [startLoc]
-    while goal not in nearby_cells:
-        nearby_cells = neighboring_cells(startLoc, wavefront_map)
-        
-        for cell in nearby_cells:
-            cell_x, cell_y = cell
-            cur_cell_value = wavefront_map[cell_x][cell_y]
-            if cur_cell_value < min_cell_value:
-                min_cell_value = cur_cell_value
-                next_cell = cell
-                
-        path.append(next_cell)
-        startLoc = next_cell
-    
-    return path
-
-
-def print_map(map):
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            # Check if the current coordinates are within the bounds of the array
-            if i < len(map) and j < len(map[0]):
-                print(map[i][j], end=" ")
-            else:
-                # If not, print an empty space
-                print(" ", end=" ")
-        print() 
+    if direction == 'right':
+        tank.on_for_degrees(left_speed=motor_speed, right_speed=-motor_speed, degrees=actual_rotational_degrees)
+    elif direction == 'left':
+        tank.on_for_degrees(left_speed=-motor_speed, right_speed=motor_speed, degrees=actual_rotational_degrees)
+    else:
+         return -1
 
 
 
-def wavefront_algorithm(world_map, goalLoc):
-    map_x = len(world_map)
-    map_y = len(world_map[0])
-    x_goal, y_goal = goalLoc
-
-
-    world_map[x_goal][y_goal] = 2
-    frontier = []
-    explored_set =set()
-    frontier.append((x_goal, y_goal)) 
-    
-
-    while frontier:
-        current_cell = frontier.pop(0)
-        explored_set.add((x_goal,  y_goal))
-        x_cur = current_cell[0]
-        y_cur = current_cell[1]
-        print("on the frontier: ",current_cell)
-        # neighbours can use the neighbouring cells function.
-        neighbors = neighboring_cells(current_cell, world_map) # check all cells
-        print("its neighbours: ", neighbors)
-        for neighbour in neighbors:
-            if neighbour not in explored_set:
-                world_map[neighbour[0]][neighbour[1]] = world_map[x_cur][y_cur] + 1
-                
-                frontier.append ((neighbour[0],  neighbour[1]))
-                explored_set.add((neighbour[0],  neighbour[1]))
-        print_map(world_map)
-
-    return world_map
 
 if __name__ == "__main__":
     # print(neighboring_cells((1,3), world_map1))
-    print(wavefront_algorithm(world_map, (1,3)))
+    world_map = wavefront_algorithm(world_map, goal)
+    print_map(world_map)
+    best_path = best_path(world_map, start)
+    print("best path: ", best_path)
+    followPath(start, start_oreintation, best_path)
+    # driveStraight(100, TILE_DISTANCE)
+    # driveStraight(100,DIAGONAL_DISTANCE)
